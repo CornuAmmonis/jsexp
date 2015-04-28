0a302118bd9decba70b3a74b08b6d03ef0575c72
Cameron Turner <cjt36@cornell.edu>
2015-04-28 01:06:25 -0400

    update softmax manifold denoise harness

--- softmaxdenoise/softmax.js
+++ softmaxdenoise/softmax.js
@@ -11,6 +11,7 @@ var ManifoldDenoise = function(config){
 
     this.uiStep = 0 >>> 0; // coerce to uint32
     this.uiTwo  = 2 >>> 0;
+    this.uiMax  = 6 >>> 0;
 
     this.mTexture1 = undefined;
     this.mTexture2 = undefined;
@@ -18,29 +19,29 @@ var ManifoldDenoise = function(config){
 
     this.feedbackMaterial = undefined;
     this.screenMaterial = undefined;
-    this.debugScreenMaterial = undefined;
 
     this.image = undefined;
 
     this.textureWidth = 1024;
-    this.textureHeight = 512;
+    this.textureHeight = 1024;
 
     // Configuration.
-    this.softf     = config.softf;
-    this.timesteps = config.timesteps;
+    this.amount = config.amount;
+    this.mix    = config.mix;
 
     this.mUniforms = {
         screenWidth:  {type: "f", value: undefined},
         screenHeight: {type: "f", value: undefined},
-        tSource:      {type: "t", value: undefined},
+        sSource:      {type: "t", value: undefined},
         iSource:      {type: "t", value: undefined},
         step:         {type: "i", value: this.uiStep},
-        softf:        {type: "f", value: this.softf}
+        amount:       {type: "f", value: this.amount},
+        mix:          {type: "f", value: this.mix}
     };
 
     this.load = function()
     {
-        this.image = THREE.ImageUtils.loadTexture('frog.jpg', THREE.UVMapping, this.init.bind(this))
+        this.image = THREE.ImageUtils.loadTexture('tetons.jpg', THREE.UVMapping, this.init.bind(this))
     };
 
     this.init = function()
@@ -79,7 +80,6 @@ var ManifoldDenoise = function(config){
 
         this.feedbackMaterial = this.getMaterial('feedbackFragmentShader');
         this.screenMaterial = this.getMaterial('screenFragmentShader');
-        this.debugScreenMaterial = this.getMaterial('debugScreenFragmentShader');
 
         var plane = new THREE.PlaneGeometry(1.0, 1.0);
         this.mScreenQuad = new THREE.Mesh(plane, this.screenMaterial);
@@ -117,43 +117,43 @@ var ManifoldDenoise = function(config){
 
     this.updateUniforms = function()
     {
-        this.mUniforms.softf.value  = this.softf;
-        this.mUniforms.step.value   = this.uiStep;
+        this.mUniforms.amount.value = this.amount;
+        this.mUniforms.mix.value    = this.mix;
     };
 
-    this.renderToTarget = function(material, tSource, target)
+    this.renderToTarget = function(material, sSource, target)
     {
         this.mScreenQuad.material = material;
-        this.mUniforms.tSource.value = tSource;
+        this.mUniforms.sSource.value = sSource;
         this.mRenderer.render(this.mScene, this.mCamera, target, true);
     };
 
     this.render = function()
     {
+        if (this.uiStep < this.uiMax) {
+            this.updateUniforms();
+            for (var i = 0; i < this.uiMax; ++i) {
+                this.mUniforms.step.value = Math.min(this.uiStep, this.uiMax);
+                console.log(this.mUniforms.step.value);
+                var pStep = this.uiStep % this.uiTwo;
 
-        this.updateUniforms();
+                if (pStep == 0) {
+                    this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.mTexture2);
+                } else if (pStep == 1) {
+                    this.renderToTarget(this.feedbackMaterial, this.mTexture2, this.mTexture1);
+                }
 
-        // Let's play triple FBO ping pong
-        // step:    1-2-3-4-5-6
-        // tSource: 1-2-2-3-3-1
-        // sSource: 3-3-1-1-2-2
-        // target:  2-1-3-2-1-3
-        for(var i = 0; i < this.timesteps; ++i) {
-
-            var pStep = this.uiStep % this.uiTwo;
-
-            if (pStep == 0) {
-                this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.mTexture1);
-            } else if (pStep == 1) {
-                this.renderToTarget(this.feedbackMaterial, this.mTexture2, this.mTexture2);
+                this.uiStep++;
             }
 
-            this.uiStep++;
+            this.mScreenQuad.material = this.screenMaterial;
+            this.mRenderer.render(this.mScene, this.mCamera);
         }
-
-        this.mScreenQuad.material = this.screenMaterial;
-        this.mRenderer.render(this.mScene, this.mCamera);
         requestAnimationFrame(this.render.bind(this));
+    };
+
+    this.resetCounter = function() {
+        this.uiStep = 0 >>> 0;
     };
 
     this.snapshot = function()
@@ -220,16 +220,13 @@ var updateUI = function(shaderId, gui, paintFlow) {
 
 window.onload = function() {
     var manifoldDenoise = new ManifoldDenoise({
-        softf: 1.0,
-        timesteps: 2
+        amount: 5.0,
+        mix: 1.0
     });
     var gui = new dat.GUI();
 
-    gui.add(manifoldDenoise, 'softf').min(0.0).max(1.0).step(0.01).name("Softness");
-    gui.add(manifoldDenoise, 'timesteps').min(1).max(10).step(1).name("Timesteps");
-
-
-    gui.remember(manifoldDenoise);
+    gui.add(manifoldDenoise, 'amount').min(0.0).max(10.0).step(0.01).name("Amount").onChange(manifoldDenoise.resetCounter.bind(manifoldDenoise));
+    gui.add(manifoldDenoise, 'mix').min(0.0).max(1.0).step(0.01).name("Mix").onChange(manifoldDenoise.resetCounter.bind(manifoldDenoise));
 
     manifoldDenoise.load();
 };
