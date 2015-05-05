205a3c54d3e6ef88be4dc9d8f78a12c860b83c74
Cameron Turner <cjt36@cornell.edu>
2015-05-05 03:12:26 -0400

    path tracer demo

--- softmaxdenoisecolor/softmax.js
+++ softmaxdenoisecolor/softmax.js
@@ -14,6 +14,8 @@ var ManifoldDenoise = function(config){
 
     this.uiChannel = 0 >>> 0;
 
+    this.time = new Date().getTime();
+
     this.mTexture1 = undefined;
     this.mTexture2 = undefined;
     this.mTexture3 = undefined;
@@ -21,11 +23,15 @@ var ManifoldDenoise = function(config){
     this.mTexture5 = undefined;
     this.mTexture6 = undefined;
     this.mTexture7 = undefined;
+    this.mTexture8 = undefined;
+    this.dummyTexture = undefined;
     this.mScreenQuad = undefined;
 
     this.feedbackMaterial = undefined;
+    this.pathTracerMaterial = undefined;
     this.sliceMaterial = undefined;
     this.screenMaterial = undefined;
+    this.accumulatorMaterial = undefined;
 
     this.image = undefined;
 
@@ -37,6 +43,7 @@ var ManifoldDenoise = function(config){
     this.mix    = config.mix;
     this.exps   = config.exps;
     this.detail = config.detail;
+    this.arate  = config.arate;
 
     this.mUniforms = {
         screenWidth:  {type: "f", value: undefined},
@@ -51,7 +58,9 @@ var ManifoldDenoise = function(config){
         amount:       {type: "f", value: this.amount},
         mix:          {type: "f", value: this.mix},
         detail:       {type: "f", value: this.detail},
-        exps:         {type: "f", value: this.exps}
+        exps:         {type: "f", value: this.exps},
+        time:         {type: "i", value: 0},
+        arate:        {type: "f", value: this.arate}
     };
 
     this.load = function()
@@ -95,9 +104,11 @@ var ManifoldDenoise = function(config){
         this.mCamera.position.z = 0;
         this.mScene.add(this.mCamera);
 
-        this.feedbackMaterial = this.getMaterial('feedbackFragmentShader');
-        this.sliceMaterial = this.getMaterial('sliceFragmentShader');
-        this.screenMaterial = this.getMaterial('screenFragmentShader');
+        this.feedbackMaterial    = this.getMaterial('feedbackFragmentShader');
+        this.pathTracerMaterial  = this.getMaterial('pathTracerFragmentShader');
+        this.sliceMaterial       = this.getMaterial('sliceFragmentShader');
+        this.screenMaterial      = this.getMaterial('screenFragmentShader');
+        this.accumulatorMaterial = this.getMaterial('accumulatorFragmentShader');
 
         var plane = new THREE.PlaneGeometry(1.0, 1.0);
         this.mScreenQuad = new THREE.Mesh(plane, this.screenMaterial);
@@ -120,6 +131,9 @@ var ManifoldDenoise = function(config){
         this.mTexture5 = this.getWrappedRenderTarget();
         this.mTexture6 = this.getWrappedRenderTarget();
         this.mTexture7 = this.getWrappedRenderTarget();
+        this.mTexture8 = this.getWrappedRenderTarget();
+        this.mTexture9 = this.getWrappedRenderTarget();
+        this.dummyTexture = this.getWrappedRenderTarget();
 
         this.mUniforms.screenWidth.value = this.textureWidth;
         this.mUniforms.screenHeight.value = this.textureHeight;
@@ -140,21 +154,25 @@ var ManifoldDenoise = function(config){
 
     this.updateUniforms = function()
     {
+        this.mUniforms.arate.value  = this.arate;
         this.mUniforms.amount.value = this.amount;
         this.mUniforms.exps.value   = this.exps;
         this.mUniforms.mix.value    = this.mix;
+        this.mUniforms.time.value   = new Date().getTime() - this.time;
     };
 
-    this.renderToTarget = function(sSource, target)
+    this.renderToTarget = function(material, iSource, sSource, target)
     {
-        this.mScreenQuad.material = this.feedbackMaterial;
+        this.mScreenQuad.material = material;
+        this.mUniforms.iSource.value = iSource;
         this.mUniforms.sSource.value = sSource;
         this.mRenderer.render(this.mScene, this.mCamera, target, true);
     };
 
-    this.sliceTarget = function(sSource0, sSource1, sSource2, target)
+    this.sliceToTarget = function(iSource, sSource0, sSource1, sSource2, target)
     {
         this.mScreenQuad.material = this.sliceMaterial;
+        this.mUniforms.iSource.value = iSource;
         this.mUniforms.sSource0.value = sSource0;
         this.mUniforms.sSource1.value = sSource1;
         this.mUniforms.sSource2.value = sSource2;
@@ -163,32 +181,42 @@ var ManifoldDenoise = function(config){
 
     this.render = function()
     {
-        if (this.uiStep < this.uiMax) {
-            this.updateUniforms();
 
-            this.mUniforms.step.value = 0;
+        this.updateUniforms();
 
-            this.mUniforms.chan.value = 0;
-            this.renderToTarget(this.mTexture1, this.mTexture2);
-            this.mUniforms.chan.value = 1;
-            this.renderToTarget(this.mTexture1, this.mTexture3);
-            this.mUniforms.chan.value = 2;
-            this.renderToTarget(this.mTexture1, this.mTexture4);
+        this.renderToTarget(this.pathTracerMaterial, this.dummyTexture, this.dummyTexture, this.mTexture1);
 
-            this.mUniforms.step.value = 1;
+        this.mUniforms.step.value = 0;
 
-            this.renderToTarget(this.mTexture2, this.mTexture5);
-            this.renderToTarget(this.mTexture3, this.mTexture6);
-            this.renderToTarget(this.mTexture4, this.mTexture7);
+        this.mUniforms.chan.value = 0;
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.dummyTexture, this.mTexture2);
+        this.mUniforms.chan.value = 1;
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.dummyTexture, this.mTexture3);
+        this.mUniforms.chan.value = 2;
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.dummyTexture, this.mTexture4);
 
-            this.sliceTarget(this.mTexture5, this.mTexture6, this.mTexture7, this.mTexture1);
+        this.mUniforms.step.value = 1;
 
-            this.uiStep++;
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.mTexture2, this.mTexture5);
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.mTexture3, this.mTexture6);
+        this.renderToTarget(this.feedbackMaterial, this.mTexture1, this.mTexture4, this.mTexture7);
+
+        this.sliceToTarget(this.mTexture1, this.mTexture5, this.mTexture6, this.mTexture7, this.mTexture2);
+
+        if (this.uiStep % 2 == 0) {
+            this.renderToTarget(this.accumulatorMaterial, this.mTexture8, this.mTexture2, this.mTexture9);
+            this.mUniforms.sSource.value = this.mTexture9;
+        } else {
+            this.renderToTarget(this.accumulatorMaterial, this.mTexture9, this.mTexture2, this.mTexture8);
+            this.mUniforms.sSource.value = this.mTexture8;
         }
+
         this.mUniforms.detail.value = this.detail;
-        this.mUniforms.sSource.value = this.mTexture1;
+        this.mUniforms.iSource.value = this.mTexture1;
         this.mScreenQuad.material = this.screenMaterial;
         this.mRenderer.render(this.mScene, this.mCamera);
+
+        this.uiStep++;
         requestAnimationFrame(this.render.bind(this));
     };
 
@@ -263,14 +291,16 @@ window.onload = function() {
         amount: 5.0,
         mix: 1.0,
         exps: 3.0,
-        detail: 0.0
+        detail: 0.0,
+        arate: 0.1
     });
     var gui = new dat.GUI();
 
-    gui.add(manifoldDenoise, 'mix').min(0.0).max(1.0).step(0.01).name("Mix").onChange(manifoldDenoise.resetCounter.bind(manifoldDenoise));
-    gui.add(manifoldDenoise, 'amount').min(0.0).max(20.0).step(0.01).name("Hardness").onChange(manifoldDenoise.resetCounter.bind(manifoldDenoise));
-    gui.add(manifoldDenoise, 'exps').min(0.0).max(5.0).step(0.01).name("Exponent").onChange(manifoldDenoise.resetCounter.bind(manifoldDenoise));
-    gui.add(manifoldDenoise, 'detail').min(0.0).max(3.0).step(0.01).name("Enhance Detail");
+    gui.add(manifoldDenoise, 'mix').min(0.0).max(1.0).step(0.01).name("Mix");
+    gui.add(manifoldDenoise, 'amount').min(0.0).max(10.0).step(0.01).name("Hardness");
+    gui.add(manifoldDenoise, 'exps').min(0.0).max(5.0).step(0.01).name("Exponent");
+    gui.add(manifoldDenoise, 'arate').min(0.0).max(0.2).step(0.001).name("Accumulator Rate");
+    gui.add(manifoldDenoise, 'detail').min(0.0).max(10.0).step(0.01).name("Enhance Detail");
 
     manifoldDenoise.load();
 };
